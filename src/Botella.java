32e73ae5dd21bb97548b2f034439e12f16d219df
ANDRES <espanaandres681@gmail.com>
2026-08-28 18:01:54 -0500

    Tercer commit, creamos el 1er constructor

--- src/Botella.java
+++ src/Botella.java
@@ -3,4 +3,11 @@ public class Botella {
     private String Tipo;
     private int CantidadDeBotellas;
     private double precio;
+
+    public Botella(String marca, String Tipo) {
+        this.marca = marca;
+        this.Tipo = Tipo;
+        this.CantidadDeBotellas = 2;
+        this.precio = 180.000;
+    }
 }
